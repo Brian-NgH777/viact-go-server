a31508bbc1f83390f2a07e09a8f84ea06b391368
Brian nguyen <brian.ng142957@gmail.com>
2021-12-21 23:08:18 +0700

    edit

--- services/fasthttp.go
+++ services/fasthttp.go
@@ -104,7 +104,7 @@ func (s *servives) FastHttp(host string, port int) {
 	s.fastHttp.POST("/webhook/snapshots", s.webhookSnapshotsHandler)
 
 
-	s.fastHttp.ServeFiles("./static/{filepath:*}",  "/home/ec2-user/viact-go-server")
+	s.fastHttp.ServeFiles("/static/{filepath:*}",  "/home/ec2-user/viact-go-server")
 
 	fasthttp.ListenAndServe(service, s.fastHttp.Handler)
 }
