ff99794f023ac431468ea10162003b837c072d74
Brian nguyen <brian.ng142957@gmail.com>
2021-12-22 21:47:43 +0700

    Edit

--- services/fasthttp.go
+++ services/fasthttp.go
@@ -209,7 +209,8 @@ func (s *servives) snapshotDeviceHandler(ctx *fasthttp.RequestCtx) {
 
 	//data, err := exec.Command("sh","cmd.sh", "get_first_frame", v.Rtsp, v.Name).Output()
 	//action get_first_frame "RTSP_LINK=$2 FILE_NAME=$3"
-	data, err := exec.Command("/usr/local/bin/action","get_first_frame", "RTSP_LINK=rtsp://admin:Viact123@192.168.92.110:554/live FILE_NAME=kk22.png").Output()
+	arg := fmt.Sprintf("RTSP_LINK=%s FILE_NAME=%s", v.Rtsp, v.Name)
+	data, err := exec.Command("/usr/local/bin/action","get_first_frame", arg).Output()
 	if err != nil {
 		fmt.Println("errerrerrerrerr", err.Error())
 		ctx.Error("Run Command failed!", fasthttp.StatusInternalServerError)
