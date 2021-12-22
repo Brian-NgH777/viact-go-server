59c3f6230b3dc8af19e081ad7d51af689c71c2d7
Brian nguyen <brian.ng142957@gmail.com>
2021-12-22 16:38:55 +0700

    Edit

--- services/fasthttp.go
+++ services/fasthttp.go
@@ -29,6 +29,11 @@ type macReq struct {
 	MacAddress string `json:"macAddress"`
 }
 
+type snapshotReq struct {
+	Name string `json:"name"`
+	Rtsp string `json:"rtsp"`
+}
+
 type webhookDeviceReq struct {
 	Data []*webhookDeviceItem `json:"data"`
 }
@@ -61,7 +66,7 @@ type repModel struct {
 }
 
 type Device struct {
-	ID            primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	ID            primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	IP            string             `json:"ip,omitempty" bson:"ip,omitempty"`
 	User          string             `json:"user,omitempty" bson:"user,omitempty"`
 	Password      string             `json:"password,omitempty" bson:"password,omitempty"`
@@ -76,6 +81,7 @@ type Device struct {
 	PTZ           string             `json:"ptz,omitempty" bson:"ptz,omitempty"`
 	Thumbnail     string             `json:"thumbnail,omitempty" bson:"thumbnail,omitempty"`
 	CameraName    string             `json:"cameraName,omitempty" bson:"cameraName,omitempty"`
+	RTMP          string             `json:"rtmp,omitempty" bson:"rtmp,omitempty"`
 	CreatedAt     time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
 	UpdatedAt     time.Time          `json:"updatedAt,omitempty" bson:"updatedAt,omitempty"`
 }
@@ -91,21 +97,28 @@ func (s *servives) FastHttp(host string, port int) {
 	service := fmt.Sprintf("%s:%d", host, port)
 
 	s.fastHttp.GET("/ping", s.pingHandler)
-	s.fastHttp.GET("/api/scan-device/list", s.listScanDeviceHandler)
-	s.fastHttp.GET("/api/scan-device", s.scanDeviceHandler) // call pythoncli for scan
-	s.fastHttp.GET("/api/snapshot", s.snapshotDeviceHandler) // call pythoncli for snapshot
-	s.fastHttp.GET("/api/device/list", s.listDeviceHandler)
 
+	// Live stream Handler
+	s.fastHttp.GET("/api/device/stream/{id}", s.streamDeviceHandler) // call pythoncli for live stream
+
+	// verification Mac Handler
 	s.fastHttp.POST("/api/mac/verification", s.verificationMacHandler)
 	s.fastHttp.POST("/api/mac/create", s.createMacHandler)
+
+	// Device Handler
+	s.fastHttp.GET("/api/device/list", s.listDeviceHandler)
 	s.fastHttp.POST("/api/devices/create", s.createDevicesHandler)
 
-	// Webhook for find list devices
+	// Webhook for find list devices Handler
+	s.fastHttp.GET("/api/scan-device/list", s.listScanDeviceHandler)
+	s.fastHttp.GET("/api/scan-device", s.scanDeviceHandler) // call pythoncli for scan
 	s.fastHttp.POST("/webhook/devices", s.webhookDevicesHandler)
 
-	// Webhook for snapshots
+	// Webhook for snapshots Handler
+	s.fastHttp.POST("/api/snapshot", s.snapshotDeviceHandler) // call pythoncli for snapshot
 	s.fastHttp.POST("/webhook/snapshots", s.webhookSnapshotsHandler)
 
+	// Serve static files
 	s.fastHttp.NotFound = fasthttp.FSHandler("/home/ec2-user/viact-go-server/static", 0)
 
 	fasthttp.ListenAndServe(service, s.fastHttp.Handler)
@@ -183,10 +196,52 @@ func (s *servives) scanDeviceHandler(ctx *fasthttp.RequestCtx) {
 
 func (s *servives) snapshotDeviceHandler(ctx *fasthttp.RequestCtx) {
 	ctx.Response.Header.Set("Content-Type", "application/json")
-	nameFile := ctx.QueryArgs().Peek("name")
 	rep := &repModel{}
 
-	rep.Data = nameFile
+	v := &snapshotReq{}
+	err := json.Unmarshal(ctx.PostBody(), v)
+	if err != nil {
+		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
+		return
+	}
+
+	arg := fmt.Sprintf("get_first_frame \"RTSP_LINK=%s FILE_NAME=%s\"", v.Rtsp, v.Name)
+	data, err := exec.Command("/usr/local/bin/action", arg).Output()
+	if err != nil {
+		ctx.Error("Run Command failed!", fasthttp.StatusInternalServerError)
+		return
+	}
+
+	rep.Data = string(data)
+	reply, _ := json.Marshal(rep)
+	ctx.SetStatusCode(200)
+	ctx.Write(reply)
+}
+
+func (s *servives) streamDeviceHandler(ctx *fasthttp.RequestCtx) {
+	ctx.Response.Header.Set("Content-Type", "application/json")
+	rep := &repModel{}
+	var d Device
+	id := ctx.UserValue("id").(string)
+	collectionDevice := s.mongo.Db.Collection("devices")
+	objID, _ := primitive.ObjectIDFromHex(id)
+	filter := bson.D{{Key: "_id", Value: objID}}
+	err := collectionDevice.FindOne(ctx, filter).Decode(&d)
+	if err != nil {
+		ctx.Error("Not found device", fasthttp.StatusInternalServerError)
+		//if err == mongo.ErrNoDocuments {
+		//	return
+		//}
+		return
+	}
+	arg := fmt.Sprintf("livestream \"RTSP_LINK=%s RTMP_LINK=%s\"", d.High, d.RTMP)
+	_, err = exec.Command("/usr/local/bin/action", arg).Output()
+	if err != nil {
+		ctx.Error("Run Command failed!", fasthttp.StatusInternalServerError)
+		return
+	}
+
+	rep.Data = d.RTMP
 	reply, _ := json.Marshal(rep)
 	ctx.SetStatusCode(200)
 	ctx.Write(reply)
@@ -197,8 +252,8 @@ func (s *servives) listDeviceHandler(ctx *fasthttp.RequestCtx) {
 	rep := &repModel{}
 	devices := []*Device{}
 
-	collectionBooking := s.mongo.Db.Collection("devices")
-	cur, err  := collectionBooking.Find(ctx, bson.D{})
+	collectionDevice := s.mongo.Db.Collection("devices")
+	cur, err := collectionDevice.Find(ctx, bson.D{})
 	if err != nil {
 		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
 		return
@@ -236,13 +291,19 @@ func (s *servives) createDevicesHandler(ctx *fasthttp.RequestCtx) {
 		return
 	}
 
-	collectionBooking := s.mongo.Db.Collection("devices")
+	now := time.Now()
+	sec := now.Unix()
+
+	collectionDevice := s.mongo.Db.Collection("devices")
 	device.IP = v.Ip
 	device.User = v.User
 	device.Password = v.Password
 	device.DeviceType = v.DeviceType
 	device.DeviceName = v.DeviceName
 	device.High = v.High
+	if len(v.High) == 0 {
+		device.High = "rtsp://admin:Viact123@192.168.92.111/live"
+	}
 	device.Medium = v.Medium
 	device.Low = v.Low
 	device.Port = v.Port
@@ -251,10 +312,11 @@ func (s *servives) createDevicesHandler(ctx *fasthttp.RequestCtx) {
 	device.PTZ = v.PTZ
 	device.Thumbnail = v.Thumbnail
 	device.CameraName = v.CameraName
+	device.RTMP = fmt.Sprintf("rtmp://54.254.0.41/live/test%d", sec)
 	device.CreatedAt = time.Now().UTC()
 	device.UpdatedAt = time.Now().UTC()
 
-	_, err = collectionBooking.InsertOne(ctx, device)
+	_, err = collectionDevice.InsertOne(ctx, device)
 	if err != nil {
 		ctx.Error(err.Error(), fasthttp.StatusInternalServerError)
 		return
@@ -312,4 +374,3 @@ func (s *servives) webhookSnapshotsHandler(ctx *fasthttp.RequestCtx) {
 	ctx.SetStatusCode(200)
 	ctx.Write(reply)
 }
-
