405b880da057cad2388b86d2c6bb977af1ffe92c
Brian nguyen <brian.ng142957@gmail.com>
2021-12-30 12:46:14 +0700

    Edit rtmp

--- services/fasthttp.go
+++ services/fasthttp.go
@@ -413,7 +413,7 @@ func (s *services) createDevicesHandler(ctx *fasthttp.RequestCtx) {
 	device.PTZ = v.PTZ
 	device.Thumbnail = v.Thumbnail
 	device.CameraName = v.CameraName
-	device.RTMP = fmt.Sprintf("rtmp://54.254.0.41/live/test%d", sec)
+	device.RTMP = fmt.Sprintf("rtmp://13.212.12.141:1935/live/%d", sec)
 	device.CreatedAt = time.Now().UTC()
 	device.UpdatedAt = time.Now().UTC()
 
